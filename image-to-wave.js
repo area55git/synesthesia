4119208314768f9701267c95a5b6f882938ae87a
ismay <ismay@ismaywolff.nl>
2018-03-21 12:41:58 +0100

    Update tests

--- image-to-wave.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const { toWav } = require('./lib/convert');
-
-const imageBuffer = fs.readFileSync(path.join(__dirname, 'images', 'input.jpg'));
-
-toWav(imageBuffer, 'image/jpeg').then(waveBuffer => {
-  fs.writeFileSync(path.join(__dirname, 'dist', 'output.wav'), waveBuffer);
-});
--- wave-to-image.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const { toImage } = require('./lib/convert');
-
-const waveBuffer = fs.readFileSync(path.join(__dirname, 'dist', 'output.wav'));
-
-toImage(waveBuffer, 1920, 1920).then(imageBuffer => {
-  fs.writeFileSync(path.join(__dirname, 'dist', 'output.png'), imageBuffer);
-});
